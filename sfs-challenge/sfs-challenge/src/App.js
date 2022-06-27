8a266bf3f8a4e8c366829cfec893a6521a0adaec
Kendall Perry <perry.kendall@gmail.com>
2022-06-27 16:55:19 -0400

    added total and checked state, and table footer for total balance

--- sfs-challenge/sfs-challenge/src/App.js
+++ sfs-challenge/sfs-challenge/src/App.js
@@ -7,16 +7,36 @@ const sfsUserData =
 
 function App() {
   const [userData, setUserData] = useState([]);
+  const [checked, setChecked] = useState(new Array(userData.length).fill(false));
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     getJSONdata();
   }, []);
 
+  // Updates the state for which users are checked, as well as the total balance for all users
+  const handleChange = (position) => {
+    const updateCheckedState = checked.map((user, idx) => 
+      idx === position ? !user : user
+    )
+    setChecked(updateCheckedState);
+
+    const totalBalance = updateCheckedState.reduce((sum, checkedState, idx) => {
+      if (checkedState) {
+        return sum + userData[idx].balance;
+      }
+      return sum;
+    }, 0)
+
+    setTotal(totalBalance);
+ }
+
   const getJSONdata = async () => {
     const response = await axios.get(sfsUserData);
     setUserData(response.data);
-    console.log("USER DATA", userData);
   };
+
+  console.log("user data", userData);
 
   return (
     <div className="App">
@@ -25,6 +45,7 @@ function App() {
       <table>
         <thead>
           <tr>
+            <th></th>
             <th>Creditor</th>
             <th>First Name</th>
             <th>Last Name</th>
@@ -34,16 +55,37 @@ function App() {
         </thead>
         <tbody>
         {userData.map((user) => {
-          return <tr key={user.id}>
+          return (
+            <tr key={user.id}>
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={handleChange}
+            />
             <td>{user.creditorName}</td>
             <td>{user.firstName}</td>
             <td>{user.lastName}</td>
             <td>{user.minPaymentPercentage}</td>
             <td>{user.balance}</td>
           </tr>
-        })};
+          )
+        })}
         </tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>Total</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>{total}</td>
+          </tr>
+        </tfoot>
       </table>
+      <>
+        <p>Total Row Count: {userData.length}</p>
+        <p>Check Row Count: </p>
+      </>
     </div>
   );
 }
