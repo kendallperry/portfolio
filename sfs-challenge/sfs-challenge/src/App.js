bfb53820b417159f235457e8a95c5cff61917d0f
Kendall Perry <perry.kendall@gmail.com>
2022-06-27 15:26:47 -0400

    added header to table

--- sfs-challenge/sfs-challenge/src/App.js
+++ sfs-challenge/sfs-challenge/src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const sfsUserData = "https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json"
+const sfsUserData =
+  "https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json";
 
 function App() {
   const [userData, setUserData] = useState([]);
@@ -14,12 +15,23 @@ function App() {
   const getJSONdata = async () => {
     const response = await axios.get(sfsUserData);
     console.log("USER DATA", response);
-  }
+  };
 
   return (
     <div className="App">
       <p>Strategic Financial Solutions</p>
       <p>Coding Challenge</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Creditor</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Min Pay %</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
