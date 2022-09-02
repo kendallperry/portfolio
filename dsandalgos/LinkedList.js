6a83a518e2311d6ddcefe60b879643a0c3349d32
Kendall Perry <perry.kendall@gmail.com>
2022-09-02 16:11:04 -0400

    linked lists under the hood

---
+++ dsandalgos/LinkedList.js
@@ -0,0 +1,15 @@
+// LINKED LISTS 
+
+// Arrays have indexes, LL don't 
+// LL have spaces in memory, arrays are in one place - contiguous place in memory (Linked Lists can be all over the place!)
+// LL has a HEAD and a TAIL for first and last item
+// Each item in the list points to the next, tail points to NULL
+
+// Big O of Linked Lists... 
+
+// What is the node? its the VALUE and the POINTER
+// Node is an object like below
+{
+    value: 4,
+    next: null // next would be another node that it points to
+}
