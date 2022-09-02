e51e164fcd92f2826c6448d0b38b4aa1fc3a30f8
Kendall Perry <perry.kendall@gmail.com>
2022-09-02 13:00:49 -0400

    more big o notes

--- dsandalgos/Big O.js
+++ dsandalgos/Big O.js
@@ -35,3 +35,30 @@ function logItems3(n) {
 }
 
 // If you would put another nested for loop, it would be n^3
+
+
+// Drop Non-Dominants 
+// Nested for loop ran O(n^2), second for loop was O(n);
+// O(n^2 + n), would just be O(n^2)
+
+function logItems4(n) {
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
+            console.log(i, j) 
+        }       
+    } 
+
+    for(let k = 0; k < n; k++) {
+        console.log(k)
+    }
+}
+
+
+// O(1)
+// This is just one operation, so it's O of 1. "Constant Time"
+// If you added n + n + n, it would be O(2), which is still O(1).
+// The MOST efficient Big O. Constant time!
+
+function addItems(n) {
+    return n + n;
+}
