6e15e47eadbf79b06c2bb9bbccf46853a55794b7
Kendall Perry <perry.kendall@gmail.com>
2022-10-31 14:24:10 -0400

    styling css tweaks

--- kendallperry/src/components/About.js
+++ kendallperry/src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
         Hello, I'm Kendall!
     <br /> <br/>
         Prior to becoming a software engineer, I was a freelance
-        musician/composer for ten years. During that time, I composed, directed,
+        musician/composer for ten years with a background in UX/UI Web Design. During that time, I composed, directed,
         and co-produced numerous original projects as a multi-instrumentalist
         and composer, including four productions with Wild Heart Dance company,
         and more recently, a concert series with the Ring of Keys at Joe's Pub
@@ -21,7 +21,10 @@ const About = () => {
         software engineer, and write code that performs beautifully with design
         and intuitive functionality in mind. Some of my interests include user
         accessibility and experience, data visualization, and working with code
-        across the full stack. As a musician, I am a composer of music. As a web
+        across the full stack. 
+        <br /> <br />
+
+        As a musician, I am a composer of music. As a web
         developer, I am a composer of code. Web development is my craft, and my
         craft is one that is always growing!
       </p>
