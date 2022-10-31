e86bf008c55081cc3d2f70344cb4acb79e4aad01
Kendall Perry <perry.kendall@gmail.com>
2022-10-31 14:35:38 -0400

    adjusted project section and indiv projects formatting;

--- kendallperry/src/components/Project.js
+++ kendallperry/src/components/Project.js
@@ -4,12 +4,17 @@ const Project = ({project}) => {
     return (
         <div id="individual-project">
             <h4>{project.title}</h4>
+            <div className="inner-project">
             <img src = {project.image} className="project-image" alt="Project" />
+            <div className="inner-project-text"> 
             <p>{project.description}</p>
             <a href={project.deployedUrl}  target="_blank" rel="noopener noreferrer">{project.deployedUrl.length ? 'Deployed Link' : null}</a>
             <br />
             <a href={project.github}  target="_blank" rel="noopener noreferrer">GitHub</a>
+            </div>
         </div>
+        </div>
+    
     )
 }
 
